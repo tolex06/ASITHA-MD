841184c559139e54a34992bdbb43baf8da414886
tolex06 <114908250+tolex06@users.noreply.github.com>
2024-11-02 01:22:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=SqJDlYaQ#yWzjycfcB2KLWCoiULrLEW--eNDnc8JdPNvbQbpiwYE",
+MONGODB: process.env.MONGODB || "mongodb+srv://Alashyt:femzotboi10@cluster0.w3h65q7.mongodb.net/?retryWrites=true&w=majority",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
